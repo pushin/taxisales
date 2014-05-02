ece68e07555d19c022b7a25d7fcecceb43cf5598
pushin <pushin.konstantin@gmail.com>
2014-05-02 06:43:48 -0700

    Install serializer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -85,6 +85,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 
             new Sensio\Bundle\BuzzBundle\SensioBuzzBundle(),
+
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
